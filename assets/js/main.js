b0b74ff54384b0f41ec6d87740cb428d8a9d6730
Matheus Crivellari <matheus.acrivellari@gmail.com>
2018-05-09 00:25:36 -0300

    Implemented client-side chat basic logic.

---
+++ assets/js/main.js
@@ -0,0 +1,171 @@
+(function (window) {
+
+	var msgPanel 	= document.getElementById('msgPanelBody');
+	var inputFld 	= document.getElementById('inputFld');
+	var sendButton 	= document.getElementById('sendButton');
+
+	/**
+	 * Strings with message templates
+	 */
+	var MessageTemplate = {
+		// Alert template
+		alert : '<div class="row alert"> \
+			        <div class="msg alert"> \
+			            <p><i class="fa fa-lightbulb-o"></i> {msg}</p> \
+			        </div>\
+			    </div>',
+
+		they : '<div class="row they">\
+					<div class="msg they">\
+						<p><strong>Stranger:</strong></p>\
+						<div>{msg}</div>\
+					</div>\
+				</div>',
+
+
+		me : '<div class="row me">\
+		    <div class="msg me">\
+		        <p><strong>Me:</strong></p>\
+		        <div>{msg}</div>\
+		    </div>\
+		</div>',
+	};
+
+	/**
+	 * Constant values for Keys
+	 */
+	var Keys = {
+		SHIFT : 16,
+		ENTER : 13,
+		RETURN : 10,
+	};
+
+	/**
+	 * Chat namespace for main chat functions
+	 * @type {Object}
+	 */
+	var Chat = {
+		// Flags namespace
+		flags : {
+			holding : {
+				shift : false,
+			},
+		},
+
+		// Render namespace
+		render : {
+			// Renders a message based on a template
+			message : function (template, msg) {
+				if(template && msg){
+					if(msg.indexOf('\n') != -1){
+						msg = msg.split('\n');
+						msg = '<p>' + msg.join('</p><p>') + '</p>';
+					}else{
+						console.log('Tem \n');
+						msg = '<p>' + msg + '</p>';
+					}
+
+					// Replaces {msg} token for desired message
+					var s = template.replace('{msg}', msg);
+					var msgDOM = $(s);
+					$(msgPanel).append(msgDOM);
+				}
+			},
+
+			// Message namespace
+			msg : {
+				me : function (msg) {
+					Chat.render.message(MessageTemplate.me,msg);
+				},
+
+				they : function (msg) {
+					Chat.render.message(MessageTemplate.they,msg);
+				},
+
+				alert : function (msg) {
+					Chat.render.message(MessageTemplate.alert,msg);
+				}
+			},
+		},
+
+		// Scroll namespace
+		scroll : {
+			top : function () {
+				msgPanel.scrollTop = 0;
+			},
+
+			bottom : function () {
+				msgPanel.scrollTop = msgPanel.scrollHeight;
+			},
+		},
+
+		/**
+		 * Callback fired when key is released
+		 * while input field is focussed.
+		 */
+		onKeyUp : function (e) {
+			if(e.which == Keys.SHIFT){
+				Chat.flags.holding.shift = false;
+			}
+
+			if(e.which == Keys.ENTER || e.which == Keys.RETURN){
+				if(Chat.flags.holding.shift){
+					// do nothing because line breaking is textarea default behaviour
+				}else{
+					e.preventDefault();
+					Chat.sendMessage();
+					return;
+				}
+			}
+		},
+
+		/**
+		 * Callback fired when key is pressed
+		 * while input field is focussed.
+		 */
+		onKeyDown : function (e) {
+			if(e.which == Keys.SHIFT){
+				Chat.flags.holding.shift = true;
+			}
+		},
+
+		/**
+		 * Sends the message through the socket
+		 * and renders it to the panel.
+		 */
+		sendMessage : function () {
+			var msgString = inputFld.value;
+			inputFld.value = '';
+			Chat.render.msg.me(msgString);
+			Chat.scroll.bottom();
+		},
+
+		/**
+		 * Callback fired when send button clicked.
+		 */
+		sendButtonClicked : function () {
+			Chat.sendMessage();
+		},
+
+		/**
+		 * Initializes Chat
+		 */
+		initialize : function () {
+			console.log('Initializing.');
+			inputFld.addEventListener('keydown', Chat.onKeyDown);
+			inputFld.addEventListener('keyup', Chat.onKeyUp);
+			sendButton.addEventListener('click', Chat.sendButtonClicked);
+		},
+	};
+
+	Chat.initialize();
+
+	/**
+	 * Exposing Chat namespace for testing
+	 * @todo Remember to remove for production
+	 */
+	window.expose = {
+		Chat : Chat,
+	}
+
+})(window);
