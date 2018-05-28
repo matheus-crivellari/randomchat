3c9800f98d9d48df54e9a16eda77f9a20a5db115
Matheus Crivellari <matheus.acrivellari@gmail.com>
2018-05-28 19:29:40 -0300

    Heroku testing.

--- server.py
+++ server.py
@@ -7,12 +7,14 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html',)
+    return render_template('index.html')
 
 @socketio.on('send_message')
 def handle_source(json_data):
-    text = json_data['message'].encode('ascii', 'ignore')
-    socketio.emit('echo', {'echo': 'Server Says: ' + text})
+    echo = json_data['message']
+    echo = 'Server Says: {}'.format(echo)
+    print('Teco ', echo)
+    socketio.emit('echo', {'echo': echo})
 
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app, debug=True)
