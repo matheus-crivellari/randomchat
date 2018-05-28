b04a4d14ffd1e030fc920badd43a450429d5f3e6
Matheus Crivellari <matheus@eadskill.com.br>
2018-05-28 12:26:40 -0300

    Testing on Heroku.

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import json
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template('index.html',)
+
+@socketio.on('send_message')
+def handle_source(json_data):
+    text = json_data['message'].encode('ascii', 'ignore')
+    socketio.emit('echo', {'echo': 'Server Says: ' + text})
+
+if __name__ == "__main__":
+    socketio.run(app)
