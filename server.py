9d3a3be21b7660d822f29d3b035c74925084fd35
Matheus Crivellari <matheus@eadskill.com.br>
2018-05-28 12:39:29 -0300

    Testing heroku.

--- server.py
+++ server.py
@@ -11,7 +11,6 @@ def index():
 
 @socketio.on('send_message')
 def handle_source(json_data):
-	print('Chegou msg')
     text = json_data['message'].encode('ascii', 'ignore')
     socketio.emit('echo', {'echo': 'Server Says: ' + text})
 
