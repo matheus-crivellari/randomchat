281617e3d3467b4e30fcc381fbbc422d4f0978ee
Matheus Crivellari <matheus.acrivellari@gmail.com>
2018-05-28 22:32:32 -0300

    Changed run.py

--- run.py
+++ run.py
@@ -7,11 +7,12 @@ from randomchat.home.views import home
 from randomchat.chat.views import chat, sio
 
 app = Flask(__name__)
+sio.init_app(app)
 app.register_blueprint(home)
 app.register_blueprint(chat)
 
-# sio.init_app(app)
 # sio.run(app, debug=True)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	# app.run(debug=True)
+	sio.run(app, debug=True)
